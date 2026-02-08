1470dba26cb76baca23787799844df0949638955
Ayama11 <143358491+Ayama11@users.noreply.github.com>
2026-02-08 04:40:26 -0800

    Add SVG icon components for GitHub, LinkedIn, and more

---
+++ components/icons.tsx
@@ -0,0 +1,38 @@
+export function IconGithub(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+      <path d="M12 .5C5.73.5.75 5.62.75 12c0 5.1 3.3 9.43 7.88 10.96.58.11.8-.26.8-.58v-2.1c-3.2.71-3.87-1.39-3.87-1.39-.52-1.36-1.28-1.72-1.28-1.72-1.05-.73.08-.72.08-.72 1.16.08 1.78 1.23 1.78 1.23 1.03 1.8 2.7 1.28 3.36.98.1-.76.4-1.28.72-1.58-2.55-.3-5.23-1.33-5.23-5.93 0-1.31.45-2.38 1.2-3.22-.12-.3-.52-1.55.12-3.23 0 0 .98-.32 3.2 1.23.93-.27 1.93-.4 2.93-.4s2 .13 2.93.4c2.22-1.55 3.2-1.23 3.2-1.23.64 1.68.24 2.93.12 3.23.75.84 1.2 1.91 1.2 3.22 0 4.61-2.69 5.63-5.25 5.93.41.36.77 1.08.77 2.18v3.23c0 .32.21.69.81.58C19.95 21.43 23.25 17.1 23.25 12 23.25 5.62 18.27.5 12 .5z" />
+    </svg>
+  );
+}
+
+export function IconLinkedIn(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+      <path d="M20.45 20.45h-3.55v-5.57c0-1.33-.02-3.04-1.85-3.04-1.85 0-2.14 1.45-2.14 2.95v5.66H9.36V9h3.41v1.56h.05c.47-.9 1.62-1.85 3.34-1.85 3.57 0 4.23 2.35 4.23 5.41v6.33zM5.34 7.43a2.06 2.06 0 1 1 0-4.12 2.06 2.06 0 0 1 0 4.12zM7.12 20.45H3.56V9h3.56v11.45z" />
+    </svg>
+  );
+}
+
+export function IconMoon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+      <path d="M21 14.5A8.5 8.5 0 0 1 9.5 3a7 7 0 1 0 11.5 11.5z" />
+    </svg>
+  );
+}
+export function IconSun(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+      <path d="M12 18a6 6 0 1 0 0-12 6 6 0 0 0 0 12zM12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M4.93 19.07l1.41-1.41M17.66 6.34l1.41-1.41" />
+    </svg>
+  );
+}
+
+export function IconSpark(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
+      <path d="M12 2l1.3 5.2L18 8.5l-4.7 1.3L12 15l-1.3-5.2L6 8.5l4.7-1.3L12 2zM19 13l.8 3.2L23 17l-3.2.8L19 21l-.8-3.2L15 17l3.2-.8L19 13z" />
+    </svg>
+  );
+}
